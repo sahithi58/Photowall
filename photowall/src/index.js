440099ddd69660363d07db815bb3af37d2f1bafb
sahithi vankayalapati <vankayalapati.s@husky.neu.edu>
2019-02-09 02:23:32 -0500

    created the react application with hello

---
+++ photowall/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = React.createElement('h1',null,"Hello User")
+
+ReactDOM.render(element,document.getElementById("root"));

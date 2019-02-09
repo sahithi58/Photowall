e32c1f2137afbafd2ece6c58cf9e5dbdf9b0396f
sahithi vankayalapati <vankayalapati.s@husky.neu.edu>
2019-02-09 02:38:12 -0500

    Little aboue jsx

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const tasks = ['take out','take snow', 'take rain']
-
-const element = <ol>
-    {/* If array has duplicate entries key as task will fail so we use index */}
-    {tasks.map((task,index) => <li key ={index}>{task}</li>)}
-    {/* <li>{task[0]}</li> */}
-</ol>
+// An element can only have one tag, with multiple sub tags in it.
+//So to have ol and h1 tag we have to just add a div tag.
+const element = 
+<div>
+    <h1>Hiii</h1>
+    <ol>
+        {tasks.map((task,index) => <li key ={index}>{task}</li>)}
+    </ol>
+</div>
 
 ReactDOM.render(element,document.getElementById("root"));

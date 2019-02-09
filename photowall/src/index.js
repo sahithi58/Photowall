43cf7abdfefd13a63d0093d059c41b7754739a89
sahithi vankayalapati <vankayalapati.s@husky.neu.edu>
2019-02-09 02:30:49 -0500

    Exploring create Element React

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = React.createElement('h1',null,"Hello User")
+const tasks = ['take out','take snow', 'take rain']
+
+const element = React.createElement('ol',null,tasks.map(
+    (task,index)=>
+    // Key should be unique.
+    React.createElement('li',{key: index},task)
+    ))
+
+// const element = React.createElement('ol',null,
+// React.createElement('li',null,"take out"),
+// React.createElement('li',null,"take snow"),
+// React.createElement('li',null,"take rain")
+// )
 
 ReactDOM.render(element,document.getElementById("root"));

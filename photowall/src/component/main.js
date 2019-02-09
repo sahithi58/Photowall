4c83be9057d8c491bb09258f96607ca9a26bc7fe
sahithi vankayalapati <vankayalapati.s@husky.neu.edu>
2019-02-09 03:14:22 -0500

    Created photo wall component

--- photowall/src/component/main.js
+++ photowall/src/component/main.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
-import List from './list'
 import Title from './title'
+import Photowall from './photowall'
+
+const posts = [{
+    id:"0",
+    description:'Avengers 1',
+    imageLink: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiihpbMma7gAhWxdd8KHdc_DNAQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D48fKIXlxaXk&psig=AOvVaw1k8NppnwJdsNFIdXKIi_3p&ust=1549786173623337"
+
+},
+{
+    id:"1",
+    description:'Avengers Age of ultron',
+    imageLink: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjanpXkma7gAhWpmeAKHTb2BcYQjRx6BAgBEAQ&url=https%3A%2F%2Fmovieweb.com%2Favengers-age-ultron-infinity-war-marvel-cinematic-universe-retrospective%2F&psig=AOvVaw0xjN7_XEjLaDvfzpDQGVpU&ust=1549786232743191"
+
+},
+{
+    id:"2",
+    description:'Avengers Infinity War',
+    imageLink:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjT84_wma7gAhXBV98KHUzXAJgQjRx6BAgBEAQ&url=https%3A%2F%2Fitunes.apple.com%2Fus%2Fmovie%2Favengers-infinity-war%2Fid1370224078&psig=AOvVaw2T-0ZhftjzR_eQxwJSgoCC&ust=1549786264007326"
+
+}
+
+]
 
 class Main extends Component{
     render(){
         return <div>
-                    <Title title={"Hello User"}/>
-                    <List tasks = {['take out', 'take rain']}/>
-                    <List tasks = {['drive away', 'do laundry']}/>
+                    <Title title={"Photowall"}/>
+                    <Photowall/>
                </div>
     }
 }
--- photowall/src/index.js
+++ photowall/src/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Main from './component/main'
-
+import './styles/stylesheet.css'
 
 ReactDOM.render(<Main/>,document.getElementById("root"));
